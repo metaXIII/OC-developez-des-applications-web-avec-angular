7643d2106a1c6b25b6e5ebff62fafd6cc6909f62
metaxiii <lehchibi.gael@pm.me>
2021-07-05 11:35:23 +0200

    add project

---
+++ 2. Construisez une application angular/mon-projet-angular/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MonPremierComponent } from './mon-premier/mon-premier.component';
+import { AppareilComponent } from './appareil/appareil.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MonPremierComponent,
+    AppareilComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
