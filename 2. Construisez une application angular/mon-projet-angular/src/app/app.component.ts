aceebd8523e00b567d499b78e66ca6334dab3321
metaxiii <lehchibi.gael@pm.me>
2021-07-05 12:31:16 +0200

    add databinding, event binding and two way binding

--- 2. Construisez une application angular/mon-projet-angular/src/app/app.component.ts
+++ 2. Construisez une application angular/mon-projet-angular/src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mon-projet-angular';
+  isAuth: boolean = false
+  appareilOne = "Machine à laver"
+  appareilTwo = "Frigo"
+  appareilThree = "Ordinateur"
+
+  constructor() {
+    setTimeout(() => {
+      this.isAuth = true
+    }, 4000);
+  }
+
+  onAllumer() {
+    console.log('On allume tout')
+  }
 }
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Cours openclassrooms
+
+Vous pouvez consulter le lien [ici](https://openclassrooms.com/fr/courses/4668271-developpez-des-applications-web-avec-angular)
